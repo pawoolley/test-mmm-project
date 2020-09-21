e753331001ec49b341c2abcf04f3a5b66821bebd
Paul Woolley <pwoolley@cloudbees.com>
2020-09-21 10:13:42 +0100

    Initial commit

---
+++ module1/src/main/java/com/cloudbees/sdm/App.java
@@ -0,0 +1,13 @@
+package com.cloudbees.sdm;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println("Hello World!");
+    }
+}
---
+++ module2/src/main/java/com/cloudbees/sdm/App.java
@@ -0,0 +1,13 @@
+package com.cloudbees.sdm;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        StringUtils.isEmpty("");
+        System.out.println("Hello World!");
+    }
+}
